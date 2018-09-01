3d23c44c9a29303852499b849176e15a30c35b85
joseda77 <tello123@hotmail.es>
2018-09-01 12:18:35 -0500

    Dojo React

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './App.css';
+import Pokemon from './Pokemon'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      pokemons: [],
+      pokedex: []
+    }
+  }
+
+  componentWillMount() {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=15")
+      .then(response => response.json())
+      .then(pokemons => this.setState({ pokemons: pokemons.results }));
+  }
+
+  catchThem = pokemon => {
+    const randy = Math.floor(Math.random() * 10)
+    randy % 2 == 0 ? this.setState({ pokedex: this.state.pokedex.concat(pokemon) }) : alert("Pokemon salvaje no fue capturado.");
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <section className="section">
+          <div className="container">
+            <div className="columns is-centered">
+              <div className="column is-10">
+                <h4 className="title is-4">
+                  Pokemons capturados.: {this.state.pokedex.length}
+                </h4>
+                <h5 className="title is-5">
+                  Atrapa al pokemon.
+                </h5>
+                <div className="pokemon-list">
+                  {this.state.pokemons.map((pokemon, index) =>
+                    <Pokemon key={index} url={pokemon.url} />)}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+
+export default App;
